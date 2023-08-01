84e63020fb2a7e84eb79f09dff535ed59a559669
EzequielK-source <programacionezequielk@gmail.com>
2023-07-31 21:18:32 -0300

    (feat) Config ejs template engine

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 const FilesRouter = require("./routes/files.routes");
 
 app.use("/files", FilesRouter);
