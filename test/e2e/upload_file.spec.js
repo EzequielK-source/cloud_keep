d31c002d4051860aa67712b3ccf417e00743331c
EzequielK-source <programacionezequielk@gmail.com>
2023-07-31 21:15:45 -0300

    (feat) Added testcases to verify upload_file template content

--- test/e2e/upload_file.spec.js
+++ test/e2e/upload_file.spec.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const cheerio = require("cheerio");
 const app = require("../../src/app");
 describe("Upload file e2e test", () => {
   /**
@@ -14,12 +15,30 @@ describe("Upload file e2e test", () => {
    * Tags {upload_file, e2e}
    */
   const ENDPOINT = "/files/upload";
-  test("Should render upload_file template", async () => {
+  describe("Should render upload_file template", () => {
     /**
      * Verify if request the endpoint with GET method render
      * the template upload_file
      */
-    const response = await request(app).get(ENDPOINT);
-    expect(response.status).toBe(200);
+    let response;
+    let $;
+    beforeAll(async () => {
+      response = await request(app).get(ENDPOINT);
+      $ = cheerio.load(response.text);
+    });
+    it("Should have status 200 ", () => {
+      expect(response.status).toBe(200);
+    });
+    it("Should exist one file input", async () => {
+      const fileInputElements = $('input[type="file"]');
+      expect(fileInputElements.length).toBeGreaterThan(0);
+    });
+
+    it("Should exist one title with the text 'Seleccione el archivo a subir'", async () => {
+      const title = $("h1").filter((index, element) => {
+        return $(element).text() === "Seleccione el archivo a subir";
+      });
+      expect(title.length).toBeGreaterThan(0);
+    });
   });
 });
