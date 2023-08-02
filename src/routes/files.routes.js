7dc389e4549b8622b05dfa73064ce6d5e03555c6
EzequielK-source <programacionezequielk@gmail.com>
2023-08-02 16:34:12 -0300

    (feat) Added post method to route /files/upload

--- src/routes/files.routes.js
+++ src/routes/files.routes.js
@@ -3,5 +3,16 @@ const router = require('express').Router();
 router
   .route('/upload')
   .get((req, res) => res.render('upload_file'))
+  .post(async (req, res) => {
+    /**
+     * Record and persist the sended files and
+     * redirect to files view
+     */
+    if (!req.files) {
+      return res.statusCode(400);
+    }
+    await saveFiles(req.files);
+    return res.redirect('/home');
+  });
 
 module.exports = router;
