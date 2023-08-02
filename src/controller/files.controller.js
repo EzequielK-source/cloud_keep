6e9bc282b8aef117eccce4ce7e385169912d6148
EzequielK-source <programacionezequielk@gmail.com>
2023-08-02 17:08:04 -0300

    (feat)File save logic is implemented in the saveFile function of the file controller

--- src/controller/files.controller.js
+++ src/controller/files.controller.js
@@ -1,5 +1,15 @@
+const path = require('path');
+
+const storageVault = path.join(__dirname, '../../storage_vault');
 module.exports = {
-  saveFiles: async (files) => {
-    console.log('saved file');
+  saveFiles: async ({ archives }) => {
+    /**
+     * Saves the files passed by parameter in the folder assigned as storage_vault
+     *
+     * @param archives - The file list
+     * @return void
+     */
+    const storagePath = path.join(storageVault, archives.name);
+    await archives.mv(storagePath);
   },
 };
