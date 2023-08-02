a748ad5e559260fde0ef3f56736ca401ab604075
EzequielK-source <programacionezequielk@gmail.com>
2023-08-02 19:04:09 -0300

    (test) Added test utils

---
+++ test/utils.js
@@ -0,0 +1,16 @@
+const File = require('../src/models/file.schema');
+
+const utils = {};
+utils.createFakeFile = async (fakePath, index) => File.create({
+  absolutePath: fakePath + index,
+  device: 'test',
+});
+
+utils.createNfakeFiles = async (n) => {
+  const promises = Array.from({ length: n }, (_, index) => utils.createFakeFile('/fake/path', index));
+  await Promise.all(promises);
+};
+utils.deleteAllFiles = async () => {
+  await File.deleteMany({});
+};
+module.exports = utils;
