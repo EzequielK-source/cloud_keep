f7cc4ede553b7ab810175ee3fc7f652520427aac
EzequielK-source <programacionezequielk@gmail.com>
2023-08-03 23:52:02 -0300

    (feat) Separated the save files logic into two functions -Separated the save files logic into two functions, one that saves an individual file and another that loops through a list of files and persists them all

--- src/modules/files/controller.js
+++ src/modules/files/controller.js
@@ -1,37 +1,50 @@
 const path = require('path');
 const File = require('./schema');
 
-module.exports = {
-  saveFiles: async ({ archives }, pathToStorage) => {
-    /**
-     * Saves the files passed by parameter in the folder assigned as storage_vault
-     * and persist it in database
-     * @param archives - The file list
-     * @return void
-     */
-    const promises = [];
-    archives.forEach((archive) => {
-      const storagePath = path.join(pathToStorage, archive.name);
-      promises.push(archive.mv(storagePath));
-      promises.push(File.create({
-        absolutePath: storagePath,
-        device: 'default',
-      }));
-    });
-    await Promise.all(promises);
-  },
-  saveFile: async ({ archives }, pathToStorage) => {
-    /**
-     * Save one file passed by parameter in the folder assigned as storage_vault
-     * and persist it in databas
-     * @param archives - The archive to save and persist
-     * @return void
-     */
-    const storagePath = path.join(pathToStorage, archives.name);
-    await archives.mv(storagePath);
-    await File.create({
-      absolutePath: storagePath,
-      device: 'default',
-    });
-  },
+const saveOneFile = async (archive, storageVault) => {
+  /**
+   * Save the file in the storage vault and persist it in the database
+   *
+   * if the file was previously saved in the database,
+   * just rewrite the file in the storage vault
+   * @param archives - The file(s) to save
+   * @param storageVault - The absolute path to storage the file(s)
+   */
+  const storagePath = path.join(storageVault, archive.name);
+  const archiveToPersist = {
+    absolutePath: storageVault,
+    device: 'mobile',
+  };
+  await archive.mv(storagePath);
+  await File.create(archiveToPersist);
 };
+const saveAllFilesInTheList = async (archiveList, storageVault) => {
+  /**
+   * Loop through the list of files using the saveOneFile function
+   * @param archiveList - List of archives
+   * @param storageVault - The absolute path to storage the file(s)
+   */
+  const promises = [];
+  archiveList.forEach((archive) => promises.push(saveOneFile(archive, storageVault)));
+
+  await Promise.all(promises);
+};
+//
+const fileController = {};
+fileController.saveAndPersistFiles = async (archive, storageVault) => {
+  /**
+   * Saves the or the file passed by parameter in the storageVault
+   * and persists them in the database
+   *
+   * If no file is sent, an exception is thrown
+   * @param archives - The file(s) to save
+   * @param storageVault - The absolute path to storage the file(s)
+   * @error - 'No se envio ningun archivo'
+   */
+  if (Array.isArray(archive)) {
+    await saveAllFilesInTheList(archive, storageVault);
+  } else {
+    await saveOneFile(archive, storageVault);
+  }
+};
+module.exports = fileController;
